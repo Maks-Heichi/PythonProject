5fcf2145200fc879bf61a076be95cd87c99cf478
Maksimka <mister.max1988@yandex.ru>
2026-03-14 15:40:51 +0500

    Commit -m

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import Category
-import Product
+from src.shop import Category, Product
 
 if __name__ == "__main__":
     product1 = Product(
@@ -13,39 +12,39 @@ if __name__ == "__main__":
     print(product1.price)
     print(product1.quantity)
 
-    print(product2.name)
-    print(product2.description)
-    print(product2.price)
-    print(product2.quantity)
-
-    print(product3.name)
-    print(product3.description)
-    print(product3.price)
-    print(product3.quantity)
-
     category1 = Category(
         "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        "Смартфоны, как средство не только коммуникации,"
+        " но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3],
     )
 
-    print(category1.name == "Смартфоны")
+    print(category1.name)
     print(category1.description)
-    print(len(category1.products))
-    print(category1.category_count)
-    print(category1.product_count)
+    print(category1.products)
 
     product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
     category2 = Category(
         "Телевизоры",
-        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        "Современный телевизор, который позволяет наслаждаться просмотром,"
+        " станет вашим другом и помощником",
         [product4],
     )
 
-    print(category2.name)
-    print(category2.description)
-    print(len(category2.products))
     print(category2.products)
+
+    product5 = Product("LG OLED 65", "65 дюймов, 4K", 200000.0, 3)
+    category2.add_product(product5)
+    print(category2.products)
+
+    product_data = {
+        "name": "Google Pixel 8",
+        "description": "128GB, Черный",
+        "price": 60000.0,
+        "quantity": 10,
+    }
+    new_product = Product.new_product(product_data)
+    print(new_product.name, new_product.price)
 
     print(Category.category_count)
     print(Category.product_count)
